4569a4bb79366d52ea1ecd1a55378dbf50dffb02
Sunjay Varma <varma.sunjay@gmail.com>
2015-05-11 12:17:55 -0400

    Added build and clean tasks. Build creates two files: dependencies.js and app.js. The former contains all the main files from bower_components and the latter contains all the files from src. Everything is uglified and transpiled as needed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,42 @@
+var es = require('event-stream');
+
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var addsrc = require('gulp-add-src');
+var iife = require("gulp-iife");
+var runSequence = require('run-sequence');
+var bowerFiles = require('bower-files')();
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+
+var gulpUtils = require('./lib/shared/gulp-utils');
+
+gulp.task('default', function(callback) {
+  return runSequence('clean', 'lint', 'test', 'build', callback);
 });
 
+gulp.task('build', function build(callback) {
+  return es.merge(
+    gulp.src(['src/**/*.js'])
+      .pipe(sourcemaps.init())
+      .pipe(iife({useStrict: false}))
+      .pipe(concat('app.js'))
+      .pipe(babel())
+      .pipe(uglify())
+      .pipe(sourcemaps.write("sourcemaps"))
+      .pipe(gulp.dest(gulpUtils.getDistPath())),
+
+    gulp.src(bowerFiles.ext('js').files)
+      .pipe(sourcemaps.init())
+      .pipe(concat('dependencies.js'))
+      .pipe(uglify())
+      .pipe(sourcemaps.write("sourcemaps"))
+      .pipe(gulp.dest(gulpUtils.getDistPath()))
+    )
+});
+
+gulp.task('clean', function clean(callback) {
+  gulpUtils.removeDistFiles(callback);
+});
