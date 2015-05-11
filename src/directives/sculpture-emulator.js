83e4d82514ca2609436f2f76757297d14f8969b7
Sunjay Varma <varma.sunjay@gmail.com>
2015-05-11 00:55:10 -0400

    An initial directive to get started

---
+++ src/directives/sculpture-emulator.js
@@ -0,0 +1,12 @@
+let module = angular.module('sculpture-emulator', ['ngMaterial']);
+
+module.directive("sculptureEmulator", () => {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/sculpture-emulator.html',
+    controllerAs: 'emulator',
+    controller: function() {
+      // Your code here
+    }
+  };
+});
