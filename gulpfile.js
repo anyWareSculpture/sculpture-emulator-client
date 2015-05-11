7ee5f287a5a026f6b6a0543841bca8d5dd960d41
Sunjay Varma <varma.sunjay@gmail.com>
2015-05-11 15:17:52 -0400

    Changed travis reporter to something more readable

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task('lint', function lint() {
 gulp.task('test', function test() {
   return gulp.src('test/**/*-test.js', {read: false})
     .pipe(mocha({
-      reporter: process.env.TRAVIS ? 'list' : 'nyan',
+      reporter: process.env.TRAVIS ? 'spec' : 'nyan',
       compilers: 'js:babel/register'
     }));
 });
