e387451b24624aa940c288b76087d0bbbd94ed9c
Jessica Jamieson <jrjamies@uwaterloo.ca>
2015-07-17 16:39:05 -0400

    Setup streaming client connection

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,60 @@
-let SculptureEmulator = require('./sculpture-emulator')
+let SculptureEmulator = require('./sculpture-emulator');
+const StreamingClient = require('@anyware/streaming-client');
 
-// Note: pass isConnected as property for now, until connecting to streaming server is implemented.
+const DEFAULT_CLIENT_CONNECTION_OPTIONS = {
+  username: "anyware",
+  password: "anyware",
+  host: "connect.shiftr.io"
+};
+
+export class EmulatorApp {
+
+  constructor() {
+    this.client = null;
+
+  }
+
+  render() {
+    React.render(
+      <SculptureEmulator game="mole" isConnected={this.client.connected}/>,
+      document.getElementById('container')
+    );
+  }
+
+  connectAndSetup(options) {
+    options = _.defaults({}, options, DEFAULT_CLIENT_CONNECTION_OPTIONS);
+
+    if(this.client) {
+      this.client.close();
+    }
+
+    this._log("Connecting using username " + options.username);
+
+    this.client = new StreamingClient(options);
+    this.client.on(StreamingClient.EVENT_CONNECT, this._onConnectionStatusChange.bind(this));
+    this.client.on(StreamingClient.EVENT_DISCONNECT, this._onConnectionStatusChange.bind(this));
+
+    this.client.on(StreamingClient.EVENT_ERROR, this._error.bind(this));
+
+    this.client.on(StreamingClient.EVENT_STATE_UPDATE, this._onStateUpdate.bind(this));
+  }
+
+  _onConnectionStatusChange() {
+    this._log(`Client Connected: ${this.client.connected}`);
+    this.render();
+  }
+
+  _log(msg) {
+    console.log(msg);
+  }
+
+  _error(error) {
+    const errorMessage = error.stack || error.message || error;
+    console.error(errorMessage);
+  }
+
 $(document).ready(() => {
-  React.render(<SculptureEmulator game="mole" isConnected={false}/>, document.getElementById('container'));
+  let emulator_app = new EmulatorApp();
+  emulator_app.connectAndSetup();
+  emulator_app.render();
 });
