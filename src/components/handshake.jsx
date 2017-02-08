25cf6003b0e72f899fc0fd340004fbd37e830014
Marius Kintel <marius@kintel.net>
2017-02-08 16:49:28 -0500

    Started on handshake cleanup.
    Logic should be moved from the sculpture-client PanelView to one of the
    logic classes in order to progress from here.

--- src/components/handshake.jsx
+++ src/components/handshake.jsx
@@ -14,9 +14,6 @@ import config from '../config';
  * Displays handshake panels, and provides a button to send a handshake.
  */
 export default class Handshake extends React.Component {
-
-  static displayName = 'Handshake';
-  // FIXME: implement proptypes
   static propTypes = {
     handshakes: React.PropTypes.object.isRequired,
     lights: React.PropTypes.object.isRequired,
@@ -27,61 +24,32 @@ export default class Handshake extends React.Component {
     super(props);
     this.sculptureActions = new SculptureActionCreator(dispatcher);
     this.panelActions = new PanelsActionCreator(dispatcher);
-    this.state = {
-      "sculpture0": {
-        isActive: false,
-        color: "user0"
-      },
-      "sculpture1": {
-        isActive: false,
-        color: "user1"
-      },
-      "sculpture2": {
-        isActive: false,
-        color: "user2"
-      }
-    };
   }
 
   activateHandshake() {
-    const user = this.props.username;
-    this.sculptureActions.sendHandshakeActivate(user);
-    let data = {};
-    data[this.props.username] = { isActive: true, color: "user0" };
-    this.setState(data);
+    this.sculptureActions.sendHandshakeActivate(this.props.username);
   }
 
   deactivateHandshake() {
-    const user = this.props.username;
-    this.sculptureActions.sendHandshakeDeactivate(user);
+    this.sculptureActions.sendHandshakeDeactivate(this.props.username);
   }
 
   render() {
     let panels = this.props.lights.get('panels');
 
     let reactPanels = [];
-    ['0','1','2'].forEach((idx) => {
-      let username = "sculpture" + (idx)
+    for (let i = 0; i < this.props.lights.panelIds.length; i++) {
+      let idx = this.props.lights.panelIds[i];
       let panel = panels.get(idx);
-      reactPanels.unshift(<Panel
-        active={false}
-        color={this.state[username].color}
-        enableToggle={false}
-        intensity={this.state[username].isActive ? 100 : 5}
-        key={idx}
-        maxintensity={this.props.lights.get("maxIntensity")}
-        panelIdx={idx} />
+      reactPanels.unshift(
+        <Panel active={false}
+               color={panel.get('color')}
+               enableToggle={false}
+               intensity={panel.get('intensity')}
+               key={i}
+               maxintensity={panels.maxintensity}/>
       );
-    });
-    reactPanels.unshift(<Panel
-      active={false}
-      color={this.state[this.props.username].color}
-      enableToggle={false}
-      intensity={this.state[this.props.username].isActive ? 100 : 5}
-      key={'3'}
-      maxintensity={this.props.lights.get("maxIntensity")}
-      panelIdx={'3'} />
-    );
+    }
 
     return (
       <div className="handshake">
