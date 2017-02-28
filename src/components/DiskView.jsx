526b9b6a0b9ae2fa80eb844109e149b4cda8eee3
Marius Kintel <marius@kintel.net>
2017-02-27 19:33:17 -0500

    Reorganized disk game, moved DiskModel to disk-game-logic, rudimentary multi-player functionality

--- src/components/DiskView.jsx
+++ src/components/DiskView.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import GAMES from 'anyware/lib/game-logic/constants/games';
 import SculptureStore from 'anyware/lib/game-logic/sculpture-store';
-import DisksActionCreator from 'anyware/lib/game-logic/actions/disks-action-creator';
-import DiskModel from 'anyware/lib/game-logic/utils/DiskModel';
-import dispatcher from '../dispatcher';
 import {sculptureStore} from '../stores';
-import config from '../config';
 import Graphics from './svg/disk-game.svg';
 
 const SingleDisk = ({position, url}) => {
@@ -30,13 +25,7 @@ export default class DiskView extends React.Component {
 
   constructor(props) {
     super(props);
-    this.diskActions = new DisksActionCreator(dispatcher);
 
-    this.physicalDisks = {
-      disk0: new DiskModel(),
-      disk1: new DiskModel(),
-      disk2: new DiskModel(),
-    };
     this.state = {
       disk0: 0,
       disk1: 0,
@@ -51,102 +40,27 @@ export default class DiskView extends React.Component {
   }
 
   componentWillMount() {
+    // FIXME: Remove on unmount
     sculptureStore.on(SculptureStore.EVENT_CHANGE, this._handleChanges.bind(this));
-    
-    // Start physical disk model
-    Object.keys(this.physicalDisks).forEach((diskId) => {
-      this.physicalDisks[diskId].start();
-      this.physicalDisks[diskId].on('position', (pos) => this.sendDiskUpdate(diskId, pos));
-    });
-    this.interval = setInterval(() => {
-      Object.keys(this.physicalDisks).forEach((key) => this.physicalDisks[key].tick());
-    }, 30);
+    sculptureStore.on(SculptureStore.EVENT_LOCAL_CHANGE, this._handleLocalChanges.bind(this));
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
-    Object.keys(this.physicalDisks).forEach((key) => this.physicalDisks[key].removeAllListeners('position'));
-  }
-
-  sendDiskUpdate(diskId, position) {
-// FIXME: Commented out this as it's handled in _handleChanges when it comes back from the store
-// Not sure if we should proactively set the state here.
-//    this.setState({[diskId]: position});
-    this.diskActions.sendDiskUpdate(diskId, { position });
-  }
-
-  resetDisks() {
-    Object.keys(this.physicalDisks).forEach((diskId) => {
-      this.physicalDisks[diskId].stop();
-      const pos = this.disks.get(diskId).get('position');
-      this.physicalDisks[diskId].position = pos;
-      this.sendDiskUpdate(diskId, pos);
-    });
   }
 
   _handleChanges(changes) {
     this.setState({active: sculptureStore.isPlayingDiskGame});
 
-    // Handle game changes
-    if (changes.hasOwnProperty('currentGame')) {
-      // Reset on start of playing the disk game and on start of games cycle
-      if (changes.currentGame === GAMES.DISK || changes.currentGame === GAMES.HANDSHAKE) {
-        setTimeout(this.resetDisks.bind(this), 0);
-      }
-    }
-
-    if (!changes.disk) return; // Only deal with disk game changes
-
-    const diskGameChanges = changes.disk;
-
     // Handle level changes
-    if (diskGameChanges.level) {
-      this.setState({level: diskGameChanges.level});
+    if (changes.disk && changes.disk.hasOwnProperty('level')) {
+      this.setState({level: changes.disk.level});
     }
+  }
 
-    // Handle disk position changes
-    if (!diskGameChanges.disks) return;
-
-    const diskChanges = diskGameChanges.disks;
-    for (const diskId of Object.keys(diskChanges)) {
-      const disk = this.disks.get(diskId);
-      const newDiskValues = diskChanges[diskId];
-
-      let position, user, targetSpeed;
-      if (newDiskValues.hasOwnProperty('position') && newDiskValues.position !== this.state[diskId]) {
-        position = newDiskValues.position;
-      }
-      else {
-        // leave position undefined because sending a position that
-        // is already set stops the disk
-
-        // FIXME: This might not be the best way of solving this:
-        // If the changes are discarded due to the above previousHardwarePosition check, and there
-        // are no other changes, we don't need to send anything as it would be redundant.
-        delete newDiskValues.position;
-        if (Object.keys(newDiskValues).length === 0) continue;
-      }
-      if (newDiskValues.hasOwnProperty('user')) {
-        user = newDiskValues.user;
-      }
-      else {
-        user = disk.getUser();
-      }
-      if (newDiskValues.hasOwnProperty('targetSpeed')) {
-        targetSpeed = newDiskValues.targetSpeed;
-      }
-      else {
-        targetSpeed = disk.getTargetSpeed();
-      }
-
-      if (targetSpeed !== undefined) {
-        this.physicalDisks[diskId].setTargetSpeed(targetSpeed);
-      }
-      if (position !== undefined) {
-        this.physicalDisks[diskId].position = position;
-        this.setState({[diskId]: position});
-      }
-      // FIXME: Update physical model "user" state?
+  _handleLocalChanges() {
+    // Handle local position state
+    for (let diskId of ['disk0', 'disk1', 'disk2']) {
+      this.setState({[diskId]: sculptureStore.getDiskPosition(diskId)});
     }
   }
 
@@ -161,10 +75,9 @@ export default class DiskView extends React.Component {
       <g display="none"><Graphics/></g>
       <g className="transformOrigin" style={{transform: `translate(${this.props.translate[0]}px, ${this.props.translate[1]}px) scale(${this.props.scale})`}}>
           <use xlinkHref="#circle"/>
-          <use xlinkHref="#disk0" style={{transformOrigin: "192px 213px ", transform: `rotate(${this.state.disk0}deg)`}}/>
-          <use xlinkHref="#disk1" style={{transformOrigin: "468.5px 426.5px", transform: `rotate(${this.state.disk1}deg)`}}/>
-          <use xlinkHref="#disk2" style={{transformOrigin: "350px 350px", transform: `rotate(${this.state.disk2}deg)`}}/>
-          <use xlinkHref={`#level${this.state.level + 1}`}/>
+          <use xlinkHref={`#level${this.state.level}-disk0`} style={{transformOrigin: "192px 213px ", transform: `rotate(${this.state.disk0}deg)`}}/>
+          <use xlinkHref={`#level${this.state.level}-disk1`} style={{transformOrigin: "468.5px 426.5px", transform: `rotate(${this.state.disk1}deg)`}}/>
+          <use xlinkHref={`#level${this.state.level}-disk2`} style={{transformOrigin: "350px 350px", transform: `rotate(${this.state.disk2}deg)`}}/>
       </g>
     </svg>;
   }
