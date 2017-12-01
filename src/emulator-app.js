3254e5946854d8f5547c4e92c255fa936f6766d8
Marius Kintel <marius@kintel.net>
2017-12-01 14:27:48 -0500

    Alone mode fix: Run sculptureStore.init()

--- src/emulator-app.js
+++ src/emulator-app.js
@@ -8,11 +8,12 @@ import ReactDOM from 'react-dom';
 import SculptureEmulator from './components/sculpture-emulator';
 import './styles';
 import config from './config';
+import {sculptureStore} from './stores';
 
 window.onload = () => {
 
   // Apply config from the global variable anyware_config
   config.applyLocalConfig(anyware_config);
-  
+  sculptureStore.init();
   ReactDOM.render(<SculptureEmulator />, document.getElementById('content'));
 }
