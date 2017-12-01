afd75fb56dac9a5068a95db1d20ebd2013e14d7d
Marius Kintel <marius@kintel.net>
2017-12-01 14:34:17 -0500

    Removed unused ABSOLUTE_TOLERANCE config

--- config.js
+++ config.js
@@ -57,7 +57,6 @@ anyware_config = {
 
   DISK_GAME: {
     SPEEDS: [6, 15, 21, 30, 72], // degrees/sec
-    ABSOLUTE_TOLERANCE: 8, // sum of degrees tolerance for the absolute disk positions
     // The intensity of the panels that the user can use to play the sequence
     CONTROL_PANEL_COLOR: 'white',
     CONTROL_PANEL_INTENSITIES: [10, 20, 30, 40, 50],
