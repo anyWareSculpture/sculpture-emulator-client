2694f449027f8fea4c299bd3e3e1f85516c56bd7
Marius Kintel <marius@kintel.net>
2018-06-17 14:03:20 -0400

    Added SimonView

--- src/components/disk-group.jsx
+++ src/components/disk-group.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DiskView from 'anyware/lib/views/DiskView';
+import SimonView from 'anyware/lib/views/SimonView';
 import DebugView from './DebugView';
 import {sculptureStore} from '../stores';
 import config from '../config';
@@ -17,6 +18,7 @@ export default class DiskGroup extends React.Component {
       <div className="disk-group">
         <div className="sculpture-screen">
           <DiskView store={sculptureStore} config={config} {...config.projectionParameters}/>
+          <SimonView store={sculptureStore} config={config} {...config.projectionParameters}/>
           {config.DEBUG.debugView && <DebugView {...config.projectionParameters}/>}
         </div>
       </div>
